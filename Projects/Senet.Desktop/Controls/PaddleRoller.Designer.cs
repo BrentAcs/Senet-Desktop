93df7916fa1020eee57022ecec0a1ba1c20f9292
Brent Acs <brent.mann@gmail.com>
2021-03-14 19:59:55 -0400

    Neeed to remove resources, can't figure out w/ UI, going to perform surgery on the files.

--- Projects/Senet.Desktop/Controls/PaddleRoller.Designer.cs
+++ Projects/Senet.Desktop/Controls/PaddleRoller.Designer.cs
@@ -29,18 +29,44 @@ namespace Senet.Desktop.Controls
     /// </summary>
     private void InitializeComponent()
     {
+      this.paddlePanel = new System.Windows.Forms.Panel();
+      this.paddle1PictureBox = new System.Windows.Forms.PictureBox();
+      this.paddlePanel.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.paddle1PictureBox)).BeginInit();
       this.SuspendLayout();
+      // 
+      // paddlePanel
+      // 
+      this.paddlePanel.Controls.Add(this.paddle1PictureBox);
+      this.paddlePanel.Location = new System.Drawing.Point(40, 47);
+      this.paddlePanel.Name = "paddlePanel";
+      this.paddlePanel.Size = new System.Drawing.Size(170, 154);
+      this.paddlePanel.TabIndex = 0;
+      // 
+      // paddle1PictureBox
+      // 
+      this.paddle1PictureBox.Location = new System.Drawing.Point(5, 5);
+      this.paddle1PictureBox.Name = "paddle1PictureBox";
+      this.paddle1PictureBox.Size = new System.Drawing.Size(24, 144);
+      this.paddle1PictureBox.TabIndex = 0;
+      this.paddle1PictureBox.TabStop = false;
       // 
       // PaddleRoller
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.paddlePanel);
       this.Name = "PaddleRoller";
       this.Size = new System.Drawing.Size(485, 322);
+      this.paddlePanel.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.paddle1PictureBox)).EndInit();
       this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Panel paddlePanel;
+    private System.Windows.Forms.PictureBox paddle1PictureBox;
   }
 }
